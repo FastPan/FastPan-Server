b064ba0dc2e704c2bd738dbd83ff58cfa46f60d2
DeveloperLzp <developer_lzp@163.com>
2016-10-26 20:55:13 +0800

    增加注册页面 完成注册和登录的表单验证（不包括remote验证）

--- fastpan/fastpan-web/src/main/java/com/stu/fastpan/web/UserController.java
+++ fastpan/fastpan-web/src/main/java/com/stu/fastpan/web/UserController.java
@@ -2,13 +2,27 @@ package com.stu.fastpan.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping ("/user")
+@RequestMapping("/user")
 public class UserController {
-	//login页面
-	@RequestMapping ("/login")
-	public String login(){
-		return "/pages/login.html";
+	// login页面
+	@RequestMapping("/login")
+	public String login() {
+		return "/pages/user/login.html";
+	}
+
+	// 注册页面
+	@RequestMapping("/register")
+	public String register() {
+		return "/pages/user/register.html";
+	}
+
+	// 注册页面
+	@RequestMapping(value="/doRegister", method=RequestMethod.POST)
+	public String doRegister() {
+		return "redirect:/main/index";
 	}
 }
