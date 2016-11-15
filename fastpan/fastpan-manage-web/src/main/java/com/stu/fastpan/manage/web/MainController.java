80bf28488a786c74f3b105c09d67f8ef45a0a16b
DeveloperLzp <developer_lzp@163.com>
2016-11-15 16:31:01 +0800

    后端管理模块首页面提交,修改后端管理页面的目录结构

---
+++ fastpan/fastpan-manage-web/src/main/java/com/stu/fastpan/manage/web/MainController.java
@@ -0,0 +1,17 @@
+package com.stu.fastpan.manage.web;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/main")
+public class MainController {
+	// index页面
+	@RequestMapping("/index")
+	public String index() {
+		return "/pages/main/index.html";
+	}
+}
