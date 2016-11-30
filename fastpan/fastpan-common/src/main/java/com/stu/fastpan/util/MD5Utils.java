a946652a08e972a8366ce10773e612237c9928a6
Eco <1043244426@qq.com>
2016-11-30 14:26:21 +0800

    修改md5注释

--- fastpan/fastpan-common/src/main/java/com/stu/fastpan/util/MD5Utils.java
+++ fastpan/fastpan-common/src/main/java/com/stu/fastpan/util/MD5Utils.java
@@ -1,4 +1,4 @@
-package com.stu.fastpan.util;
+﻿package com.stu.fastpan.util;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -27,7 +27,7 @@ public class MD5Utils {
 			md.update(source);
 			byte tmp[] = md.digest(); // MD5 的计算结果是一个 128 位的长整数，
 										// 用字节表示就是 16 个字节
-			char str[] = new char[16 * 2]; // 每个字节用 16 进制表示的话，使用两个字符，
+			/*char str[] = new char[16 * 2]; // 每个字节用 16 进制表示的话，使用两个字符，
 											// 所以表示成 16 进制需要 32 个字符
 			int k = 0; // 表示转换结果中对应的字符位置
 			for (int i = 0; i < 16; i++) { // 从第一个字节开始，对 MD5 的每一个字节
@@ -39,7 +39,8 @@ public class MD5Utils {
 				str[k++] = hexDigits[byte0 & 0xf]; // 取字节中低 4 位的数字转换
 			}
 			s = new String(str); // 换后的结果转换为字符串
-			s=byteArrayToHex(tmp);
+*/		
+            s=byteArrayToHex(tmp);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
